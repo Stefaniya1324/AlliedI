5d5f05ce2a12743d5cd7de6fe5aaba07f4769d6a
Stefaniya1324 <44589645+Stefaniya1324@users.noreply.github.com>
2021-05-19 22:35:11 -0400

    Add files via upload

---
+++ assets/data/categories.ts
@@ -0,0 +1,81 @@
+export default {
+    items: [{
+        id: 'category1',
+        title: 'Binnun Winery',
+        wines: [{
+            id: 'category1Wine1',
+            poster: 'http://alliedimporters.com/wp-content/uploads/2018/01/BN_Reserve2016_USA.png',
+        }, {
+            id: 'category1Wine2',
+            poster: 'http://alliedimporters.com/wp-content/uploads/2018/02/Bin-Nun-Cuvee-2017.png',
+        }]
+    }, {
+        id: 'category2',
+        title: 'Borgo Reale',
+        wines: [{
+            id: 'category2Wine1',
+            poster: 'http://alliedimporters.com/wp-content/uploads/2021/03/Borgo-Reale-Pinot-Noir-2016-bottle-shot.png',
+        }, {
+            id: 'category2Wine2',
+            poster: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Rose.png',
+        }, {
+            id: 'category2Wine3',
+            poster: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Montepulciano-Dabruzzo.png',
+        }, {
+            id: 'category2Wine4',
+            poster: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Pinot-Grigio-2013.png',
+        }]
+    }, {
+        id: 'category3',
+        title: 'Dalton',
+        wines: [{
+            id: 'category3Wine1',
+            poster: 'http://alliedimporters.com/wp-content/uploads/2021/03/Alma-Rose.png',
+        }, {
+            id: 'category3Wine2',
+            poster: 'http://alliedimporters.com/wp-content/uploads/2018/02/Wine-Alma-Red.png',
+        }, {
+            id: 'category3Wine3',
+            poster: 'http://alliedimporters.com/wp-content/uploads/2017/09/Dalton-Reserve-Shiraz-2013-Img.png',
+        }, {
+            id: 'category3Wine4',
+            poster: 'http://alliedimporters.com/wp-content/uploads/2018/02/alma_ivory_2017.png',
+        }]
+    }, {
+        id: 'category4',
+        title: 'Gofna',
+        wines: [{
+            id: 'category4Wine1',
+            poster: 'http://alliedimporters.com/wp-content/uploads/2016/03/Gofna-Chardonnay-Cab-Sauv-New.png',
+        }, {
+            id: 'category4Wine2',
+            poster: 'http://alliedimporters.com/wp-content/uploads/2016/03/Gofna-Petit-Verdot.png',
+        }, {
+            id: 'category4Wine3',
+            poster: 'http://alliedimporters.com/wp-content/uploads/2016/03/Gofna-Pinot-Noir-NEW.png',
+        }]
+    }, {
+        id: 'category5',
+        title: '2Bar',
+        wines: [{
+            id: 'category5Spirit1',
+            poster: 'http://alliedimporters.com/wp-content/uploads/2021/03/Bottled-Transparent.png',
+        }, {
+            id: 'category5Spirit2',
+            poster: 'http://alliedimporters.com/wp-content/uploads/2021/03/Transparent.png',
+        }]
+    }, {
+        id: 'category6',
+        title: 'B.R. Distilling Company',
+        wines: [{
+            id: 'category6Spirit1',
+            poster: 'http://alliedimporters.com/wp-content/uploads/2021/03/Juke-Joint.png',
+        }, {
+            id: 'category6Spirit2',
+            poster: 'http://alliedimporters.com/wp-content/uploads/2018/02/Single-Barrel.png',
+        }, {
+            id: 'category6Spirit3',
+            poster: 'http://alliedimporters.com/wp-content/uploads/2018/02/Riverset.png',
+        }]
+    }]
+}
