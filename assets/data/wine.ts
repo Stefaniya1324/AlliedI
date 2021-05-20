2bb31ced7bab4e593b98aace40995dff46e3b20d
Stefaniya1324 <44589645+Stefaniya1324@users.noreply.github.com>
2021-05-19 22:33:20 -0400

    Add files via upload

---
+++ assets/data/wine.ts
@@ -0,0 +1,116 @@
+export default {
+    items: [{
+    //bottles 
+    //category1
+    id: 'category1Wine1',
+    title: 'BIN NUN RESERVE',
+    origin: 'Judean Hills, Izrael',
+    year: 2015, 
+    rating: 4.3,
+    
+    seasons: {
+        items: [{
+            id: 'category1Wine1',
+            name: 'Reserve Cabernet Sauvignon',
+            front: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-f-label.jpg',
+            back: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-b-label.jpg',
+            sell: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Chianti-Riserva.pdf',
+            poster: 'http://alliedimporters.com/wp-content/uploads/2018/01/BN_Reserve2016_USA.png',
+            }]
+    }},
+
+    {
+    id: 'category1Wine2',
+    title: 'BIN NUN CUVEE',
+    origin: 'Judean Hills, Izrael',
+    year: 2017, 
+    rating: 3.9,
+        
+    seasons: {
+        items: [{
+            id: 'category1Wine2',
+            name: 'Reserve Cabernet Sauvignon',
+            front: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-f-label.jpg',
+            back: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-b-label.jpg',
+            sell: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Chianti-Riserva.pdf',
+            poster: 'http://alliedimporters.com/wp-content/uploads/2018/02/Bin-Nun-Cuvee-2017.png',
+                }]
+    }},
+    
+    //category2
+    {
+    id: 'category2Wine1',
+    title: 'BORGO REALE PINOT NOIR',
+    origin: 'Puglia, Italy',
+    year: 2019, 
+    rating: 3.2,
+                
+    seasons: {
+        items: [{
+            id: 'category2Wine1',
+            name: 'Cantine del Borgo Reale',
+            front: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-f-label.jpg',
+            back: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-b-label.jpg',
+            sell: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Chianti-Riserva.pdf',
+            poster: 'http://alliedimporters.com/wp-content/uploads/2021/03/Borgo-Reale-Pinot-Noir-2016-bottle-shot.png',
+                }]
+    }},
+    {
+        id: 'category2Wine2',
+        title: 'BORGO REALE ROSE',
+        origin: 'Puglia, Italy',
+        year: 2018, 
+        rating: 3.7,
+        
+        
+        seasons: {
+            items: [{
+                id: 'category2Wine2',
+                name: 'Cantine del Borgo Reale',
+                front: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-f-label.jpg',
+                back: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-b-label.jpg',
+                sell: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Chianti-Riserva.pdf',
+                poster: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Rose.png',
+                }]
+        }},
+
+    {
+        id: 'category2Wine3',
+        title: 'BORGO REALE MONTEPULCIANO DABRUZZO',
+        origin: 'Puglia, Italy',
+        year: 2019, 
+        rating: 3.5,
+        
+        
+        seasons: {
+            items: [{
+                id: 'category2Wine3',
+                name: 'Cantine del Borgo Reale',
+                front: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-f-label.jpg',
+                back: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-b-label.jpg',
+                sell: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Chianti-Riserva.pdf',
+                poster: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Montepulciano-Dabruzzo.png',
+                }]
+        }},
+
+    {
+        id: 'category2Wine4',
+        title: 'BORGO REALE PINOT GRIGIO',
+        origin: 'Puglia, Italy',
+        year: 2013, 
+        rating: 3.4,
+        
+        
+        seasons: {
+            items: [{
+                id: 'category2Wine4',
+                name: 'Pinot Grigio Puglia',
+                front: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-f-label.jpg',
+                back: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-b-label.jpg',
+                sell: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Chianti-Riserva.pdf',
+                poster: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Pinot-Grigio-2013.png',
+                }]
+        }},
+
+    ]
+}
